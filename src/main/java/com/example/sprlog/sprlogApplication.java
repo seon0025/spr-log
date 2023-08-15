f6152c5e26ca60a78293e567415debb0110bd576
선진 <s22013@gsm.hs.kr>
2023-08-15 22:39:37 +0900

    API

---
+++ src/main/java/com/example/sprlog/sprlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sprlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(sprlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprlog/sprlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class sprlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
