25b61693e1f2d5133d3d16a2d5638ae7e6042a83
선진 <s22013@gsm.hs.kr>
2023-08-16 23:03:26 +0900

    domain, repo

---
+++ src/main/java/com/example/sprlog/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.sprlog.repository;
+
+import com.example.sprlog.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findALl();
+}
